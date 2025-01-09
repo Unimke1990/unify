b0473aa18d92f30187fae7bddd662168720a7605
agim <agimagba1990@gmail.com>
2025-01-09 14:32:12 +0100

    getting ready to launch on Render

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,4 @@ from app import create_app
 unify_app = create_app()
 
 if __name__ == '__main__':
-    unify_app.run(host='127.0.0.1', port=5000, debug=True)
+    unify_app.run(host='0.0.0.0', port=5000, debug=True)
