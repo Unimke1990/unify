f2a649722d1f2df2bf3a9af25df5de2cee9ff5c5
agim <agimagba1990@gmail.com>
2025-01-09 16:08:21 +0100

    Fix import statements for all files

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from app import db
+from backend.app import db
 from flask_login import UserMixin
 import bcrypt
 from datetime import datetime, timezone
--- backend/user_routes.py
+++ backend/user_routes.py
@@ -1,6 +1,6 @@
 from flask_login import login_user, logout_user, login_required, current_user
 from flask import request, render_template, url_for, redirect, flash
-from models import Users, Contacts, Reminder
+from backend.models import Users, Group, Contacts, Reminder
 from sqlalchemy.exc import IntegrityError
 from flask_bcrypt import Bcrypt
 import re
